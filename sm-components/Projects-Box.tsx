4f1878fd60ec540c76bf5d6d965dc047c1f9d399
smalakargh <supriyomic@gmail.com>
2025-07-26 10:59:50 +0530

    Map function added02 in #Project-section

--- sm-components/Projects-Box.tsx
+++ sm-components/Projects-Box.tsx
@@ -1,16 +1,31 @@
+'use client'
 import Image from 'next/image';
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 type Props = {
   imgLink: string;
   imgName: string;
   iconLinks?: string[];
 }
-function ProjectsBox({imgLink,imgName,iconLinks}:Props) {
-  const DynamicResponsive = (index: number) => {
-  const base = 10 + index * 12;
-  const md = 10 + index * 20;
-  return `left-[${base}px] md:left-[${md}px]`;
+
+
+const useMediaQuery = (query: string): boolean => {
+  const [matches, setMatches] = useState(false);
+
+  useEffect(() => {
+    const media = window.matchMedia(query);
+    setMatches(media.matches);
+
+    const handler = (e: MediaQueryListEvent) => setMatches(e.matches);
+    media.addEventListener('change', handler);
+
+    return () => media.removeEventListener('change', handler);
+  }, [query]);
+
+  return matches;
 };
+
+function ProjectsBox({imgLink,imgName,iconLinks}:Props) {
+  const isMd = useMediaQuery('(min-width: 768px)');
   return (
     <main className='m-4'>
       <div className='w-[250px] h-[150px] md:w-[500px] md:h-[300px] p-4 rounded-sm bg-neutral-500 relative flex-center overflow-hidden group'>
@@ -23,14 +38,17 @@ function ProjectsBox({imgLink,imgName,iconLinks}:Props) {
         </div>
         <div className='absolute left-1 bottom-8 md:left-4 md:bottom-12 flex'>
            <div className='flex relative'>
-              {iconLinks?.map((icon,index)=>(
+              {iconLinks?.map((icon,index)=>{
+                const left = isMd ? 10 + index * 20 : 10 + index * 12;
+                return(
                 <span
                 key={index}
-                className={`bg-slate-600 w-[23px] h-[23px] md:w-[35px] md:h-[35px] p-[6px] rounded-full border border-white absolute ${DynamicResponsive(index)}`}
+                className={`bg-slate-600 w-[23px] h-[23px] md:w-[35px] md:h-[35px] p-[6px] rounded-full border border-white absolute`}
+                style={{ left: `${left}px` }}
                 >
-                  <Image src={icon} alt={`icon-${index+1}`} fill className='p-[5px]'/>
+                  <Image src={icon} alt={`icon-${index+1}`} fill className='p-[4px]'/>
                 </span>
-              ))}
+              )})}
            </div>
         </div>
       </div>
